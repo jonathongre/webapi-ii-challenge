edd48df443d12350c2d575ff0c0bc5be460bda5c
Jonathon Green <jonathongre@gmail.com>
2019-09-03 11:36:44 -0500

    POST works

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -29,5 +29,22 @@ router.get('/:id', (req, res) => {
     });
 });
 
+//Post - add new posts
+router.post('/', (req, res) => {
+    const newPost = req.body
+    if (newPost.title && newPost.contents) {
+        db.insert(newPost)
+            .then(postNew => {
+                db.find()
+                    .then(posts => {
+                        res.json(posts)
+                    })
+            })
+            .catch(err => {
+                res.status(400).json({ error: 'Title and contents required.'})
+            })
+    }
+})
+
 
 module.exports = router; 
