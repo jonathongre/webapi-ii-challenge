c680775361e7c77e4ca96e6c70ee4caf9a027995
Jonathon Green <jonathongre@gmail.com>
2019-09-03 14:05:45 -0500

    GET and POST comments works

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -29,6 +29,18 @@ router.get('/:id', (req, res) => {
     });
 });
 
+//Get Comments
+router.get('/:id/comments', (req, res) => {
+    const { id } = req.params;
+    db.findPostComments(id)
+    .then(post => {
+        res.status(200).json(post)
+    })
+    .catch(err => {
+        res.status(500).json({ error: "Couldn't retrieve Comments"})
+    })
+});
+
 //Post - add new posts
 router.post('/', (req, res) => {
     const newPost = req.body
@@ -41,10 +53,55 @@ router.post('/', (req, res) => {
                     })
             })
             .catch(err => {
-                res.status(400).json({ error: 'Title and contents required.'})
+                res.status(400).json({ error: 'Please provide title and contents for the post.'})
             })
     }
 })
+
+//Post Comments
+router.post('/:id/comments', (req, res) => {
+    db.findById(req.params.id)
+    .then(result => {
+      if (result.length === 0) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      } else {
+        if (!req.body.text || !req.body.text.trim()) {
+          res
+            .status(400)
+            .json({
+              message: "Please provide 'text' field to create comment."
+            });
+        } else {
+          const comment = {
+            text: req.body.text,
+            post_id: req.params.id
+          };
+          
+            db.insertComment(comment)
+            .then(val => {
+              res.status(201).json(comment);
+            })
+            .catch(err => {
+              res
+                .status(500)
+                .json({
+                  message:
+                    "A server server error occured when saving the comment to the database."
+                });
+            });
+        }
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({
+          message: "There was an error looking up a post with the specified ID"
+        });
+    });
+});
 
 //Put - update post
 router.put('/:id', (req, res) => {
@@ -68,5 +125,30 @@ router.put('/:id', (req, res) => {
         res.status(400).json({ errorMessage: "Title and contents required." });
     }
 })
+
+//Delete a post
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id).then(post => {
+        const deletedPost = post;
+        db.remove(id)
+            .then(countDeleted => {
+                if (countDeleted) {
+                    db.find()
+                    .then(posts => {
+                        res.json(posts);
+                    })
+                } else {
+                    res.status(404).json({ message: 'The post with that ID not found.' });
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ error: 'That post could not be deleted.' });
+            })
+    }).catch(err => {
+        res.status(500).json({ error: 'Could not get post.' });
+    })
+});
+
 
 module.exports = router; 
